3f999a17fa54625eaf41164f2663492ce0149932
Alisson <alisson.barbozalima@gmail.com>
2019-09-14 17:36:59 -0300

    Refactor reader to not end untill complete the job

--- reader.js
+++ reader.js
@@ -3,23 +3,24 @@ const cheerio = require('cheerio')
 const { parentPort } = require('worker_threads');
 
 parentPort.on('message', (msg)=>{
-    request({url: msg},async function (error, response, html){
+    console.log('Requesting in URL ' + msg)
+    request({url: msg},async (error, response, html) => {    
         //In case if you'r asking yourself if this is really async, it is but will depends on the time that we get the answer    
-        if(!error){            
+        if(!error){
+            console.log('Request was success!!')
             var $ = cheerio.load(html)            
-            exitFunc($('img').map(async (i, e)=>{                                            
+            $('img').map((i, e)=>{                                            
                 var src = $(e).attr('src')
                 if(src.startsWith('http')){
-                    await parentPort.postMessage(src)    
+                    parentPort.postMessage(src)
                 }else{
                     console.log('não tem http')
                 }
-                
-            }))
+            })
         }else{
             console.log('Something wrong: ' + error)
         }
-    })  
+    })
 })
 
 function exitFunc(){
