cda5b1b22fbfcc2bb71d358ca5ba912767a78da2
Alisson <alisson.barbozalima@gmail.com>
2019-09-16 16:32:35 -0300

    Add definitive downloader

--- downloader.js
+++ downloader.js
@@ -1,28 +1,11 @@
 const request = require('request')
 const fs = require('fs')
 const { parentPort } = require('worker_threads')
-var NameGenerator = require('name-generator')
 
-var namer = new NameGenerator()
 
 
 parentPort.on('message', function(url){
     request(url).pipe(fs.createWriteStream(Math.random().toString(36).substring(7))).on('close', ()=> {
-        parentPort.postMessage('message', 'job finished daddy')
+        parentPort.postMessage('message', 'I"m done')
     })
-    /*
-    request({url: msg},async function (error, response){
-        //In case if you'r asking yourself if this is really async, it is but will depends on the time that we get the answer    
-        if(!error){
-            var $ = cheerio.load(response.body)                    
-            exitFunc($('img').map(async (i, e)=>{                                            
-                var src = $(e).attr('src')
-                parentPort.postMessage(src)
-            }))
-        }
-    })*/  
 })
-
-function exitFunc(){
-    process.exit(1)
-}
