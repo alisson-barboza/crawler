6827569ff56d312af5aee234953eac1d5e995b4e
Alisson <alisson.barbozalima@gmail.com>
2019-09-16 16:35:20 -0300

    Refactoring reader to deal with half-path imgs

--- reader.js
+++ reader.js
@@ -3,26 +3,22 @@ const cheerio = require('cheerio')
 const { parentPort } = require('worker_threads');
 
 parentPort.on('message', (msg)=>{
-    console.log('Requesting in URL ' + msg)
     request({url: msg},async (error, response, html) => {    
         //In case if you'r asking yourself if this is really async, it is but will depends on the time that we get the answer    
         if(!error){
-            console.log('Request was success!!')
             var $ = cheerio.load(html)            
             $('img').map((i, e)=>{                                            
-                var src = $(e).attr('src')
+                var src = $(e).attr('src')                
                 if(src.startsWith('http')){
                     parentPort.postMessage(src)
                 }else{
-                    console.log('não tem http')
+                    var http = msg
+                    var imgLink = src
+                    parentPort.postMessage(msg+imgLink)
                 }
             })
         }else{
-            console.log('Something wrong: ' + error)
+            console.log('Something went wrong: ' + error)
         }
     })
 })
-
-function exitFunc(){
-    process.exit(1)
-}
