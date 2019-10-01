e7b2058b6992722b58d7bf5060f2b71505ac8ea6
Felipe Santana <felipe-sbatista@outlook.com>
2019-10-01 17:35:19 -0300

    Lint improvements

--- downloader.js
+++ downloader.js
@@ -4,8 +4,8 @@ const { parentPort } = require('worker_threads')
 
 
 
-parentPort.on('message', function(url){
-    request(url).pipe(fs.createWriteStream(Math.random().toString(36).substring(7))).on('close', ()=> {
-        parentPort.postMessage('message', 'I"m done')
-    })
-})
+parentPort.on('message', function (url) {
+    request(url).pipe(fs.createWriteStream(Math.random().toString(36).substring(7))).on('close', () => {
+        parentPort.postMessage('message', 'I\'m done')
+    });
+});
